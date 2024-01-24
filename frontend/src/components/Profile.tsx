0bd68767eeda1d6636609bba248f6fcee7d13786
Sudipto das <sudiptodas877@gmail.com>
2024-01-24 18:55:40 +0530

    added Loader in Posts component

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -6,26 +6,34 @@ import { calculateTimeAgo } from "../utils/timeUtils";
 const Profile = () => {
     const user = useRecoilValue(userState)
     const [myPosts, setMyPosts] = useState<Post[]>([])
-
+    const [loading, setLoading] = useState(true)
     const [activeComponent, setActiveComponent] = useState('posts')
     const fetchUserPosts = async () => {
-        const response = await fetch('http://localhost:8001/post/myposts', {
-            method: 'GET',
-            headers: {
-                Authorization: "Bearer " + localStorage.getItem("token"),
-                'Content-Type': 'application/json',
-            }
-        })
-        if (response.ok) {
-            const data = await response.json()
-            const postsWithTime = data.map((post: { created_at: string; }) => (
-                {
-                    ...post,
-                    timeAgo: calculateTimeAgo(post.created_at)
+        try {
+            const response = await fetch('http://localhost:8001/post/myposts', {
+                method: 'GET',
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("token"),
+                    'Content-Type': 'application/json',
                 }
-            ))
-            setMyPosts(postsWithTime)
+            })
+            if (response.ok) {
+                const data = await response.json()
+                const postsWithTime = data.map((post: { created_at: string; }) => (
+                    {
+                        ...post,
+                        timeAgo: calculateTimeAgo(post.created_at)
+                    }
+                ))
+                setMyPosts(postsWithTime)
+
+            }
+        } catch (error) {
+
+        } finally {
+            setLoading(false)
         }
+
     }
     useEffect(() => {
         fetchUserPosts()
@@ -47,26 +55,35 @@ const Profile = () => {
             <button className="font-bold hover:border-b-2" onClick={() => setActiveComponent('followers')}>Followers</button>
         </div>
         <div className="mt-10">
-            {activeComponent === 'posts' && <Posts posts={myPosts} />}
+            {activeComponent === 'posts' && <Posts posts={myPosts} loading={loading} />}
             {activeComponent === 'following' && <Following user={user} />}
             {activeComponent === 'followers' && <Followers user={user} />}
         </div>
     </>
 }
-const Posts = ({ posts }: { posts: Post[] }) => {
+const Posts = ({ posts, loading }: { posts: Post[], loading: boolean }) => {
     return <div className="w-full m-auto  flex flex-col md:w-2/4 mb-10" >
-        {posts.length === 0 ? (
+        {loading ? (
             <div className="text-center text-gray-600 mt-10">
-                User has not posted anything yet.
+                Loading posts...
             </div>
-        ) :
-            (posts.map(post => (
-                <div key={post._id} className="rounded-xl shadow-xl mt-10 px-10 py-4 border">
-                    <div className="mb-2 font-medium text-2xl">{post.userId.username}</div>
-                    <div>{post.description}</div>
-                    <div className="text-sm text-right">{post.timeAgo}</div>
-                </div>
-            )))}
+        ) : (
+            <>
+                {posts.length === 0 ? (
+                    <div className="text-center text-gray-600 mt-10">
+                        User has not posted anything yet.
+                    </div>
+                ) : (
+                    posts.map(post => (
+                        <div key={post._id} className="rounded-xl shadow-xl mt-10 px-10 py-4 border">
+                            <div className="mb-2 font-medium text-2xl">{post.userId.username}</div>
+                            <div>{post.description}</div>
+                            <div className="text-sm text-right">{post.timeAgo}</div>
+                        </div>
+                    ))
+                )}
+            </>
+        )}
     </div>
 }
 const Followers = ({ user }: { user: { isLoading: boolean; user: User | null; } }) => {
